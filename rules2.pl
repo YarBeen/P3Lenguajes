d601438116f888527302624dc135ef9c8d5f33a3
Yarman Charpentier <yarman2002@gmail.com>
2023-10-21 17:33:33 -0600

    Some rules

--- rules2.pl
+++ rules2.pl
@@ -6,4 +6,7 @@ spread_desease(X,Y):- infects(X,Y).
 spread_desease(X,Y):- infects(X,Z),spread_desease(Z,Y).
 suspicious(X):- (hasSymptons(X); (wasInContact(X,Y), infected(Y))).
 mediumLowSuspicious(X) :- (wasInContact(X,Y), hasSymptons(Y),(not(usesMask(X));not(usesMask(Y)))).
-LowSuspicious(X) :- wasInContact(X,Y), hasSymptons(Y), (usesMask(X); usesMask(Y)).
+lowSuspicious(X) :- wasInContact(X,Y), hasSymptons(Y), (usesMask(X); usesMask(Y)).
+workAtHome(X) :- infected(X); suspicious(X); (worksWith(X,Y),hasSymptons(Y)).
+allVaccines(X) :- firstDosis(X), secondDosis(X).
+canTravel(X) :- (allVaccines(X);(firstDosis(X),negativeAntigens(X))),not(infected(X)). 
