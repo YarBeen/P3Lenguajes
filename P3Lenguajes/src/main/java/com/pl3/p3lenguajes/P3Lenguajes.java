c2f9ce126a1279a10bf98682ec4bed5cfa9a977a
Yarman Charpentier <yarman2002@gmail.com>
2023-10-21 21:13:38 -0600

    Vaccine consulter

--- P3Lenguajes/src/main/java/com/pl3/p3lenguajes/P3Lenguajes.java
+++ P3Lenguajes/src/main/java/com/pl3/p3lenguajes/P3Lenguajes.java
@@ -1,8 +1,9 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  */
-
 package com.pl3.p3lenguajes;
+
+import com.pl3.p3lenguajes.Consulters.vaccinesConsulter;
 import org.jpl7.*;
 import java.io.File;
 import org.jpl7.Query;
@@ -15,19 +16,23 @@ public class P3Lenguajes {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
-        String prologQuery = "consult('C:\\\\Users\\\\User\\\\Desktop\\\\Pruebas.pl')";
-Query consultQuery = new Query(prologQuery);
-if (consultQuery.hasSolution()){
-    System.out.println("Correct");
-}
-    Query queryDePrueba = new Query("es_perro(dblacky)");
-     Query queryDePrueba2 = new Query("es_perro(blacky)");
-  if(queryDePrueba.hasSolution()){
-      System.out.println("true");
-  }
-  else{
-            System.out.println("false");
+        String prologQuery = "consult('C:\\\\final_rules.pl')";
+        Query consultQuery = new Query(prologQuery);
+        if (consultQuery.hasSolution()) {
+            System.out.println("Correct");
+        }
+        vaccinesConsulter consulter = new vaccinesConsulter();
+//        consulter.vaccineByAge("eduardo");
+//        consulter.vaccineByAge("juan");
+//        consulter.vaccineByAge("roxas");
+//        System.out.println("Todas");
+//        consulter.vaccineByAge();
+//        consulter.recommendedReforceVaccine("empty");
+//        consulter.recommendedReforceVaccine("eduardo");
+//        consulter.recommendedReforceVaccine("juan");
+//        consulter.recommendedReforceVaccine("maria");
+           consulter.recommendedReforceVaccine();
+           consulter.vacunasPorPaciente("eduardo");
 
-  }
     }
 }
